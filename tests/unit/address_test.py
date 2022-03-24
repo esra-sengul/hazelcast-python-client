be41b52d06fa22d66f5f7edf53a5934a059b9910
esra-sengul <95536784+esra-sengul@users.noreply.github.com>
2022-03-25 01:32:36 +0300

    Update address_test.py

--- tests/unit/address_test.py
+++ tests/unit/address_test.py
@@ -11,35 +11,6 @@ class AddressHelperTest(unittest.TestCase):
     default_port = 5701
     default_port_count = 3
 
-    def test_v4_address_without_port(self):
-        self._validate_without_port(self.v4_address, self.v4_address)
-
-    def test_v6_address_with_port(self):
-        self._validate_with_port(
-            "[" + self.v6_address + "]:" + str(self.port), self.v6_address, self.port
-        )
-
-    def test_v6_address_without_port(self):
-        self._validate_without_port(self.v6_address, self.v6_address)
-
-    def test_v6_address_without_port_with_brackets(self):
-        self._validate_without_port("[" + self.v6_address + "]", self.v6_address)
-
-    def test_localhost_with_port(self):
-        self._validate_with_port(self.localhost + ":" + str(self.port), self.localhost, self.port)
-
-    def test_localhost_without_port(self):
-        self._validate_without_port(self.localhost, self.localhost)
-
-    def _validate_with_port(self, address, host, port):
-        primaries, secondaries = AddressHelper.get_possible_addresses(address)
-        self.assertEqual(1, len(primaries))
-        self.assertEqual(0, len(secondaries))
-
-        address = primaries[0]
-        self.assertEqual(host, address.host)
-        self.assertEqual(port, address.port)
-
     def _validate_without_port(self, address, host):
         primaries, secondaries = AddressHelper.get_possible_addresses(address)
         self.assertEqual(1, len(primaries))
